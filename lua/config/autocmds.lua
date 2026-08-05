e5371479e57aa1e37c915fdec3feabd3c248ed02
dyq94310 <dyq94310@foxmail.com>
2026-08-05 10:22:08 +0800

    feat:anto save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,24 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local autosave_group = vim.api.nvim_create_augroup("user_autosave", { clear = true })
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "BufLeave", "FocusLost" }, {
+  group = autosave_group,
+  callback = function(args)
+    local buf = args.buf
+    if not vim.api.nvim_buf_is_valid(buf)
+      or vim.bo[buf].buftype ~= ""
+      or not vim.bo[buf].modifiable
+      or vim.bo[buf].readonly
+      or vim.api.nvim_buf_get_name(buf) == ""
+      or not vim.bo[buf].modified then
+      return
+    end
+
+    vim.api.nvim_buf_call(buf, function()
+      vim.cmd("silent update")
+    end)
+  end,
+})
